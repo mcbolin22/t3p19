350a30d3b3e4fcde46385274b26174417125824f
Colin <mcbolin@hotmail.com>
2024-07-30 12:06:53 +1000

    fix: database URL env processed properly

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 async function databaseConnect(){
     let databaseURL = process.env.DATABASE_URL || "mongodb://localhost:27017/blogDB";
